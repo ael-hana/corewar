abb7c9cf43014547df3d3cd7bf0f3b1d7a651f94
thomasLeMeur <t.lemeur94@gmail.com>
2016-03-10 23:55:04 +0100

    change help message in main.c and target name in makefile

--- compilo/srcs/main.c
+++ compilo/srcs/main.c
@@ -12,23 +12,64 @@
 
 #include "compilo.h"
 
-static int	no_args(void)
+extern t_op	g_op_tab[17];
+
+static void	print_instrus(void)
 {
-	ft_putendl("Usage : ./asm [-a] <sourcefile.s ...>");
-	ft_putstr("\t -a : Instead of creating a .cor file, outputs a stripped ");
-	ft_putendl("and annoted version of the code to the standard output");
+	int	i;
+
+	i = -1;
+	while (g_op_tab[++i].instr_name)
+	{
+		ft_printf("\t\t\"%s\" - %d arg(s)\n",
+		g_op_tab[i].instr_name, g_op_tab[i].params_nb);
+	}
+}
+
+static int	my_help(void)
+{
+	ft_putendl("Usage : ./asm [-at] <sourcefile.s ...>\n");
+	ft_putendl("\t-a\n\t\tInstead of creating a .cor file, outputs a stripped");
+	ft_putendl("\t\tand annoted version of the code to the standard output\n");
+	ft_putendl("\t-t\n\t\tPrint this help and quit\n");
+	ft_putendl("Be carreful, the expected syntax is restrictive\n");
+	ft_printf("\tA comment begins by a character in \"%s\"\n\n", COMMENT_CHARS);
+	ft_putendl("\tA name and a comment have to be given before the labels and");
+	ft_putendl("\tthe instructions :");
+	ft_printf("\t\tName : \"%s\" followed by \"The name\"\
+	(max %d characters)\n", NAME_CMD_STRING, PROG_NAME_LENGTH);
+	ft_printf("\t\tComment : \"%s\" followed by \"The comment\"\
+	(max %d characters)\n\n", COMMENT_CMD_STRING, COMMENT_LENGTH);
+	ft_putendl("\tMany labels can't be on the same line & have the same name");
+	ft_printf("\tIts name has to be created with the characters : \"%s\"\n",
+	LABEL_CHARS);
+	ft_printf("\tand it has to be directly followed by the character : '%c'",
+	LABEL_CHAR);
+	ft_putendl("\n\n\tInstructions :");
+	print_instrus();
+	ft_putendl("\tMany instructions can't be on the same line, label names");
+	ft_printf("\thave to exist and ech argument are separated by a '%c'\n",
+	SEPARATOR_CHAR);
 	return (0);
 }
 
 static int	check_flag(int ac, char **av)
 {
 	int	i;
+	int	opt;
 
 	i = 0;
+	opt = 0;
 	while (++i < ac)
-		if (!ft_strcmp(av[i], "-a"))
-			return (1);
-	return (0);
+	{
+		if (*av[i] != '-' || av[i][1] == '-')
+			continue ;
+		if (is_in_buf('h', av[i] + 1))
+			return (2);
+		if (is_in_buf('a', av[i] + 1))
+			opt = 1;
+	}
+	return (opt);
 }
 
 static void	print_end_comment(char *file_name, int ret, int opt)
@@ -51,12 +92,14 @@ int			main(int ac, char **av)
 	int	len;
 
 	if (ac == 1)
-		return (no_args());
-	opt = check_flag(ac, av);
+		return (my_help());
+	if ((opt = check_flag(ac, av)) == 2)
+		return (my_help());
 	i = 0;
 	while (++i < ac)
 	{
-		if (!ft_strcmp(av[i], "-a"))
+		if (*av[i] == '-' && av[i][1] != '-'
+			&& is_in_buf('a', av[i] + 1))
 			continue ;
 		len = ft_strlen(av[i]);
 		if (av[i][len - 2] != '.' || av[i][len - 1] != 's')
