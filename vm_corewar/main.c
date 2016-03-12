712c2b5fab5597a99b15620e7b7097fbadd439c6
Anasse EL-HANANI <ael-hana@e2r10p18.42.fr>
2016-03-12 18:55:03 +0100

    parsing tested

--- vm_corewar/main.c
+++ vm_corewar/main.c
@@ -6,7 +6,7 @@
 /*   By: ecousine <ecousine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/11 17:10:38 by ecousine          #+#    #+#             */
-/*   Updated: 2016/03/11 17:30:57 by ecousine         ###   ########.fr       */
+/*   Updated: 2016/03/12 17:47:06 by ael-hana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@ void	init_data(t_env *data)
 
 int		main(int ac, char **av)
 {
-	t_env		data;
+//	t_env		data;
 
-	init_data(&data);
-	parse_flags(ac, av, &data);
+//	init_data(&data);
+//	parse_flags(ac, av, &data);
+	ft_parssing(av[1]);
+	return (0);
 }
