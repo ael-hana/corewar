fab12ff169151897138f449172bfb6abb99a89bd
Thomas LE MEUR <tle-meur@e2r12p16.42.fr>
2016-03-17 15:23:19 +0100

    asm finish and cleaned dir

--- compilo/tests/argus/errors/error_14_nb_-+_nothing.s
+++
@@ -1,8 +0,0 @@
-.comment "I'M ALIIIIVE"
-.name "zork"
-
-l2:		sti r1, %:live, %-
-		and r1, %0, r1
-
-live:	live %1
-		zjmp %:live
--- compilo/tests/labels/errors/error_01_manylabels_onaline.s
+++
@@ -1,9 +0,0 @@
-.comment "I'M ALIIIIVE"
-.name "zork"
-
-l2:		l3:
-		sti r1, %:live, %1
-		and r1, %0, r1
-
-live:	live %1
-		zjmp %:live
