5f563ca8b281189027fe90fa1acef3ec8082821f
Eloi <e.cousinet@gmail.com>
2016-03-18 15:45:39 +0100

    process

--- vm_corewar/process.c
+++ vm_corewar/process.c
@@ -6,7 +6,7 @@
 /*   By: ecousine <ecousine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/13 17:28:24 by ecousine          #+#    #+#             */
-/*   Updated: 2016/03/18 02:03:07 by ecousine         ###   ########.fr       */
+/*   Updated: 2016/03/18 03:39:57 by ecousine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ t_process	*create_process(t_process *father_process, int n, int position)
 		new_process->op = 0;
 		new_process->carry = 0;
 		ft_bzero(new_process->reg, sizeof(int) * 16);
-		new_process->reg[0] = 0xffff - n;
+		new_process->reg[0] = n;
 		new_process->alive = 1;
 		new_process->last_alive = 0;
 	}
