bbd588011960b61eef49e8583dcfa1c9146117b1
Eloi COUSINET <ecousine@e2r12p17.42.fr>
2016-03-31 20:36:11 +0200

    frre

--- vm/lst_del.c
+++ vm/lst_del.c
@@ -6,7 +6,7 @@
 /*   By: ecousine <ecousine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/31 18:55:40 by ecousine          #+#    #+#             */
-/*   Updated: 2016/03/31 19:16:48 by ecousine         ###   ########.fr       */
+/*   Updated: 2016/03/31 19:50:03 by ecousine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ void free_player(void *player, size_t size)
 void	free_process(void *process, size_t size)
 {
 	(void)size;
+	free_player(((t_process*)(process))->player, sizeof(t_header*));
 	free(process);
-//	free_player(((t_process*)(process))->player, sizeof(t_header*));
 }
