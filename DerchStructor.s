11f7c46ce7bcfe3b3b880cb90d35419880ea019d
Thomas LE MEUR <tle-meur@e2r11p20.42.fr>
2016-04-01 15:57:27 +0200

    end ?

--- DerchStructor.s
+++ DerchStructor.s
@@ -1,9 +1,6 @@
 .name "DerchStructor"
 .comment "All you motherfuckers are gonna pay. You are the ones who are the ball-lickers. We're gonna fuck your mothers while you watch and cry like little, whiny bitches. We're gonna make you eat our shit, then shit out our shit, and then eat yours shit that's made up of our shit that we made you eat. Then you're all you motherfucks are next. Love, STAE."
 
-#		ld			%0, r1
-#		zjmp		%-511
-
 init:
 		sti			r1, %:alive, %1
 		sti			r1, %:alive, %9
